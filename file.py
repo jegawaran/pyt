780234673cecce57f7910f7afa2a1dc9e55669f4
jegawaran <jagatheesjothi@gmail.com>
2017-11-30 17:51:45 +0530

    changes in file
    changes in file

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
 name="jegatheeswaran"
 f= open('jega.txt','r')
 #f.write('hi this is file \n')
-#f.write('here we can use11111111111 ' + name + ' words')
+#f.write('here we can use11111111111jjjjjjjjegatheeswaran ' + name + ' words')
 
 for line in f:
     splitt = line.split(' ')
