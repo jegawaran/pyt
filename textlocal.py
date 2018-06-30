9cd53460cc6a3ea823f6da95e6b093054388f7b2
Jegatheeswaran Sundaravadivel <jagatheesjothi@gmail.com>
2018-06-30 17:30:31 +0530

    python send SMS
    This is textlocal API to send SMS in Python

---
+++ textlocal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+ 
+import urllib.request
+import urllib.parse
+ 
+def sendSMS(apikey, numbers, sender, message):
+    data =  urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers,
+        'message' : message, 'sender': sender})
+    data = data.encode('utf-8')
+    request = urllib.request.Request("https://api.textlocal.in/send/?")
+    f = urllib.request.urlopen(request, data)
+    fr = f.read()
+    return(fr)
+ 
+resp =  sendSMS('api', 'mobileno',
+    'TXTLCL', 'Hi Padman,this is jegatheeswaran i am sending this sms by python')
+print (resp)
